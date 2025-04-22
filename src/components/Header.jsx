ded7ca2c88e6c242865eeea27b1748cd70351cbe
Benard Kaniaru <kaniarubenard623@gmailcom>
2025-04-22 13:09:52 +0300

    added link for the apps source code on the header

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -12,7 +12,7 @@ export default function Header({
 }) {
   return (
     <header className="flex items-center p-4 bg-gray-800 w-full fixed left-0 right-0 top-0 justify-between z-12">
-      <p className="text-blue-500 font-bold md:text-xl lg:text-2xl w-24 md:w-32 lg:w-60">
+      <p className="text-white font-bold md:text-xl lg:text-2xl w-24 md:w-32 lg:w-60">
         AngaPro
       </p>
       <SearchBar
@@ -27,7 +27,16 @@ export default function Header({
       <nav className="w-20 md:w-40 flex justify-end px-4">
         <ul>
           <li>
-            <img className="w-8" src={githubLogo} alt="github logo" />
+            <a
+              href="https://github.com/BenaKaniaru/WeatherApp.git"
+              target="_blank"
+              className="relative group hover:cursor-pointer "
+            >
+              <img className="w-8" src="/github.png" alt="github logo" />
+              <span className="absolute text-white text-sm w-32 -left-14 top-12 opacity-0 group-hover:opacity-100 transition-opacity px-2 py-1">
+                Click for Source Code
+              </span>
+            </a>
           </li>
         </ul>
       </nav>
